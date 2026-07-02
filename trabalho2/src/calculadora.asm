ee790e1deb9e7b2b625379ea2131fbe5d5c65824
Willyan Marques <willyanmarquesmelo@gmail.com>
2026-07-02 16:34:19 -0300

    arquivo io.asm

--- trabalho2/src/calculadora.asm
+++ trabalho2/src/calculadora.asm
@@ -1,13 +1,33 @@
 section .data
+; Bem-vindo. Digite seu nome:
+; Hola, <nome do usuario>, bem-vindo ao programa de CALC IA-32
+; Vai trabalhar com 16 ou 32 bits (digite 0 para 16, e 1 para 32):
+
+;ESCOLHA UMA OPÇÃO: 
+;- 1: SOMA 
+;- 2: SUBTRACAO 
+;- 3: MULTIPLICACAO 
+;- 4: DIVISAO 
+;- 5: EXPONENCIACAO 
+;- 6: MOD 
+;- 7: SAIR
 
 section .text
     global _start
+
+	; Funções para operações
 	extern adicao
 	extern subtracao
 	extern multiplicacao
 	extern divisao
 	extern modulo
 	extern exponenciacao
+
+	; Funções de IO
+    extern print_string
+    extern ler_string
+    extern ler_int32
+    extern ler_int16
 
 _start:
     ; Syscall de saída (sys_exit)
---
+++ trabalho2/src/io.asm
@@ -0,0 +1,39 @@
+section .data
+;ESCOLHA UMA OPÇÃO: 
+;- 1: SOMA 
+;- 2: SUBTRACAO 
+;- 3: MULTIPLICACAO 
+;- 4: DIVISAO 
+;- 5: EXPONENCIACAO 
+;- 6: MOD 
+;- 7: SAIR
+
+section .text
+    global print_string
+    global ler_string
+    global ler_int32
+    global ler_int16
+
+; Recebe via pilha: [ponteiro da string] e [quantidade de bytes]
+print_string:
+    ret
+
+ler_string:
+    ret
+
+ler_int32:
+    ret
+
+ler_int16:
+    ret
+
+exibir_menu:
+;ESCOLHA UMA OPÇÃO: 
+;- 1: SOMA 
+;- 2: SUBTRACAO 
+;- 3: MULTIPLICACAO 
+;- 4: DIVISAO 
+;- 5: EXPONENCIACAO 
+;- 6: MOD 
+;- 7: SAIR
+    ret
